7ede3d7594420a7dd004e99e14b8b41ae1c7d8b0
deyvsonpolish <deyvsonpolish@gmail.com>
2026-04-06 15:14:52 +0200

    auto release

--- reg.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM release.bat v1.95
-set VERSION=%1
-gh release create %VERSION% "C:\Users\dawid\Desktop\IdeaProjects\BedWarsPlugin\target\BedWarsPlugin-0.0.0.jar" --title "BedWarsPlugin %VERSION%" --notes "Dodano nowe funkcje i poprawki"
-pause
